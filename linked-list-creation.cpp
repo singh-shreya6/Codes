65dbf69c9f3dfe0383559d80a08a19add7d51c69
Shreya Singh <singh.shreya8@gmail.com>
2018-07-11 17:18:29 +0530

    Create linked-list-creation.cpp

---
+++ linked-list-creation.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+struct node
+{
+    ll data;
+    node* next;
+};
+node* head=NULL;
+
+void insert(ll val)
+{
+    node* temp=new node;
+    temp->data=val;
+    temp->next=NULL;
+    if(head==NULL)
+    head=temp;
+    else
+    {
+      node* ptr=head;
+        while(ptr->next!=NULL)
+        {
+            ptr=ptr->next;
+        }
+        ptr->next=temp;
+    }
+}
+void print()
+{
+    node* ptr=head;
+    while(ptr!=NULL)
+    {
+        cout<<ptr->data<<" ";
+        ptr=ptr->next;
+    }
+}
+
+int main()
+{
+    ll n;
+    cin>>n;
+    ll val;
+    for(ll i=0;i<n;i++)
+    {
+        cin>>val;
+        insert(val);
+    }
+    print();
+return 0;
+}
