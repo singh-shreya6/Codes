5024adee8043c6959b57b9bfe984899096ad41a1
Shreya Singh <singh.shreya8@gmail.com>
2018-07-18 15:09:28 +0530

    Create insertion in BST.cpp

---
+++ insertion in BST.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <cstddef>
+	
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  
+    void preOrder(Node *root) {
+
+          if( root == NULL )
+              return;
+
+          std::cout << root->data << " ";
+
+          preOrder(root->left);
+          preOrder(root->right);
+      }
+      class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+Node * insert(Node * root, int data) {
+        
+        if(root==NULL)
+        {
+            Node* temp=new Node(data);
+            root=temp;
+        }
+      if(data<root->data)
+            root->left=insert(root->left,data);
+       if(data>root->data)
+           root->right=insert(root->right,data);
+        return root;
+    }
+};
+int main() {
+    
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+	myTree.preOrder(root);
+    return 0;
+}
