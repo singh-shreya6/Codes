a551d8ee37c730798b79ec1bd05315a69eae5182
Shreya Singh <singh.shreya8@gmail.com>
2018-07-22 00:39:35 +0530

    Create longest-common-increasing-subsequence.cpp

---
+++ longest-common-increasing-subsequence.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n;
+        cin>>n;
+        ll a[n];
+        for(ll i=0;i<n;i++)
+            cin>>a[i];
+        ll m;
+        cin>>m;
+        ll b[m];
+        for(ll i=0;i<m;i++)
+            cin>>b[i];
+        ll arr[m]={0};
+        for(ll i=0;i<n;i++)
+        {
+            ll c=0;
+            for(ll j=0;j<m;j++)
+            {
+                if(a[i]==b[j])
+                {
+                    if(arr[j]<c+1)
+                        arr[j]=c+1;
+                }
+                else if(a[i]>b[j])
+                {
+                   if(arr[j]>c)
+                       c=arr[j];
+                }  
+            }  
+        }
+       ll mx=LLONG_MIN;
+       for(ll i=0;i<m;i++)
+       {
+           mx=max(arr[i],mx);
+       } 
+        cout<<mx<<endl;
+    }
+    return 0;
+}
