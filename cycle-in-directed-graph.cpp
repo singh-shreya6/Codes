eb2d31bf33059de3d5184895d1212f67a7096540
Shreya Singh <singh.shreya8@gmail.com>
2018-08-22 20:54:40 +0530

    Update cycle-in-directed-graph.cpp

--- cycle-in-directed-graph.cpp
+++ cycle-in-directed-graph.cpp
@@ -1,53 +1,73 @@
-//Cycle in a directed Graph
-
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-#define faster ios_base::sync_with_stdio(false);cin.tie(NULL);
-typedef long long int ll;
 
+class Graph
+{
+    int V;
+    list<int> *adj;
+public :
+    Graph(int V);
+    void addEdge(int v,int w);
+    bool isCyclic();
+};
+Graph::Graph(int V)
+{
+    this->V = V;
+    adj = new list<int>[V];
+}
+void Graph::addEdge(int v,int w)
+{
+    adj[v].push_back(w);
+}
 int main()
 {
-    faster
-    ll n,m;
-    cin>>n>>m;
-    vector<ll> adj[n];
-    ll x,y;
-    for(ll i=0;i<m;i++)
+    int T;
+    cin>>T;
+    while(T--)
     {
-        cin>>x>>y;
-        adj[x].push_back(y);
+        int _size,N;
+        cin>>_size>>N;
+        Graph *g = new Graph(_size);
+        for(int i=0;i<N;i++)
+        {
+            int u,v;
+            cin>>u>>v;
+            g->addEdge(u,v);
+        }
+        cout<<g->isCyclic()<<endl;
     }
-    bool visited[n];
-    set<ll> s;
-    stack<ll> st;
-    for(ll j=0;j<n;j++)
+}
+
+}
+bool dfs(int v, bool visited[],set<int> s,list<int> adj[])
+{
+    if(visited[v] == false)
     {
-        s.clear();
-        st.empty();
+        visited[v] = true;
+        s.insert(v);
+        list<int>::iterator it;
+        for(it= adj[v].begin(); it != adj[v].end(); it++)
+        {
+            if (visited[*it]==false && dfs(*it, visited,s,adj)==true)
+                return true;
+            else if (s.find(*it)!=s.end())
+                return true;
+        }
+ 
+    }
+    return false;
+}
+
+bool Graph :: isCyclic()
+{
+    bool visited[V];
+    set<int> s;
+    for(int i = 0; i < V; i++)
+    {
         memset(visited,false,sizeof(visited));
-        st.push(j);
-        visited[j]=true;
-        while(!st.empty())
-        {
-        ll u=st.top();
-        s.insert(u);
-        st.pop();
-        for(ll i=0;i<adj[u].size();i++)
-        {
-            ll vertex=adj[u][i];
-            if(s.find(vertex)!=s.end())
-            {
-                cout<<"Cycle found"<<endl;
-                return 0;
-            }
-            if(visited[vertex]==false)
-            {
-                    st.push(vertex);
-                    visited[vertex]=true;
-            }
-        }
-        }
-    }  
-    cout<<"Cycle not found"<<endl;
-    return 0;
+        s.clear();
+        if (dfs(i, visited,s,adj)==true)
+            return true;
+    }
+    return false;
 }
