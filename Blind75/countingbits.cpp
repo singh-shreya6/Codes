6bd5042c128c48b0aa5e6e059004c3002384f2cb
Shreya Singh <singh.shreya8@gmail.com>
2021-07-25 21:34:17 +0530

    Create countingbits.cpp

---
+++ Blind75/countingbits.cpp
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/counting-bits
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans;
+        for(int i = 0; i<=n;i++){
+            ans.push_back(__builtin_popcountll(i));
+        }
+        return ans;       
+    }
+};
