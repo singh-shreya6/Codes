8f0f18cd803fa638c913d0a84ef4f6369f88ae6b
Shreya Singh <singh.shreya8@gmail.com>
2022-04-18 21:37:39 +0530

    Create ContainsDuplicate.cpp

---
+++ Blind75/ContainsDuplicate.cpp
@@ -0,0 +1,19 @@
+/*
+https://leetcode.com/problems/contains-duplicate
+*/
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        set<int> st;
+        for(int i = 0; i < n ;i ++){
+            if(st.find(nums[i]) == st.end()){
+              st.insert(nums[i]);
+            } else {
+                return true;
+            }
+        }
+        return false;
+        
+    }
+};
